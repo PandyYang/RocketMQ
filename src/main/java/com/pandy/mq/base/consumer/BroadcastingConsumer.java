b369c7cb46393222dd78efe50a0d7f8792c0920d
PandyYang <yangpandy@gmail.com>
2021-12-05 00:58:11 +0800

    消费者的默认负载均衡和广播模式

--- src/main/java/com/pandy/mq/base/consumer/BroadcastingConsumer.java
+++ src/main/java/com/pandy/mq/base/consumer/BroadcastingConsumer.java
@@ -6,14 +6,15 @@ import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
 import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
 import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.common.message.MessageExt;
+import org.apache.rocketmq.common.protocol.heartbeat.MessageModel;
 
 import java.util.List;
 
 /**
  * 消息的接收者
- * 默认负载均衡
+ * 广播模式
  */
-public class Consumer {
+public class BroadcastingConsumer {
     public static void main(String[] args) throws MQClientException {
         /**
          * 1.创建消费者Consumer，制定消费者组名
@@ -27,6 +28,7 @@ public class Consumer {
         consumer.setNamesrvAddr("192.168.174.174:9876;192.168.174.173:9876");
 
         consumer.subscribe("base", "Tag2");
+        consumer.setMessageModel(MessageModel.BROADCASTING);
 
         consumer.registerMessageListener(new MessageListenerConcurrently() {
             // 接收消息内容的方法
